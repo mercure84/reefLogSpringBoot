4c1ee347bcb6490aad36bbeaf154c279bc582bae
Julien M <julien.marcesse@gmail.com>
2020-01-25 00:43:53 +0100

    bean Member and some properties

--- src/test/java/com/reeflog/reefLogApi/ReefLogApiApplicationTests.java
+++ src/test/java/com/reeflog/reefLogApi/ReefLogApiApplicationTests.java
@@ -6,8 +6,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ReefLogApiApplicationTests {
 
+
 	@Test
-	void contextLoads() {
+	public void NoTest(){
+
+		System.out.println("Hello World, there is no tests here !");
+
 	}
+
 
 }
