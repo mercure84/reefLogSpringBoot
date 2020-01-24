5d93e04b54318a39e0259bbb8eccabd6b29fdd3c
Julien M <julien.marcesse@gmail.com>
2020-01-24 23:23:19 +0100

    first commit socle spring boot

---
+++ src/main/java/com/reeflog/reefLogApi/ReefLogApiApplication.java
@@ -0,0 +1,13 @@
+package com.reeflog.reefLogApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReefLogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReefLogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reeflog/reefLogApi/ReefLogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reeflog.reefLogApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReefLogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
