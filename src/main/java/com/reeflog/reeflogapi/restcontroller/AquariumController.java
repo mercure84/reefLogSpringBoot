8f055820bc4cc714a0840d6511aac6ee32a0423e
Julien M <julien.marcesse@gmail.com>
2020-01-25 17:22:15 +0100

    add simple ReefAquarium

--- src/main/java/com/reeflog/reeflogapi/restcontroller/AquariumController.java
+++ src/main/java/com/reeflog/reeflogapi/restcontroller/AquariumController.java
@@ -1,9 +1,11 @@
 package com.reeflog.reeflogapi.restcontroller;
 
 
+import com.reeflog.reeflogapi.beans.Member;
 import com.reeflog.reeflogapi.beans.ReefAquarium;
 import com.reeflog.reeflogapi.beans.helpers.ReefAquariumForm;
 import com.reeflog.reeflogapi.repository.AquariumRepository;
+import com.reeflog.reeflogapi.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,13 +18,23 @@ public class AquariumController {
     @Autowired
     AquariumRepository aquariumRepository;
 
+    @Autowired
+    MemberRepository memberRepository;
+
 
     @PostMapping(value = "/api/addNewReefAquarium")
-    public ReefAquarium addNewReefAquarium(@RequestHeader("Authorization") String token, @RequestBody ReefAquariumForm ReefAquariumForm){
-        ReefAquarium reefAquarium = new ReefAquarium();
+    public ReefAquarium addNewReefAquarium(@RequestHeader("Authorization") String token, @RequestBody ReefAquariumForm reefAquariumForm){
+        ReefAquarium newReefAquarium = new ReefAquarium();
 
-
-        return reefAquarium;
+        Member member = memberRepository.findById(reefAquariumForm.getMemberId());
+        newReefAquarium.setMember(member);
+        newReefAquarium.setName(reefAquariumForm.getName());
+        newReefAquarium.setLength(reefAquariumForm.getLength());
+        newReefAquarium.setHeight(reefAquariumForm.getHeight());
+        newReefAquarium.setWidth(reefAquariumForm.getWidth());
+        newReefAquarium.setSumpVolume(reefAquariumForm.getSumpVolume());
+        aquariumRepository.save(newReefAquarium);
+        return newReefAquarium;
 
     }
 
