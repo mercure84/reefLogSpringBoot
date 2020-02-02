23b65c17cebfdcd496227d75d0cd147f0e7f27eb
Julien M <julien.marcesse@gmail.com>
2020-02-02 11:30:45 +0100

    add method info build service

--- src/main/java/com/reeflog/reeflogapi/ReefLogApiApplication.java
+++ src/main/java/com/reeflog/reeflogapi/ReefLogApiApplication.java
@@ -3,11 +3,13 @@ package com.reeflog.reeflogapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class ReefLogApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReefLogApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ReefLogApiApplication.class, args);
+    }
 
 }
