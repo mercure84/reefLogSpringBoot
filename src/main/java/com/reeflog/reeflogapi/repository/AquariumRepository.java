dcaeb9ecfaafe7ea3c62194e8c29a697228add64
Julien M <julien.marcesse@gmail.com>
2020-03-01 20:35:24 +0100

    new controller for aquarium details list by member

--- src/main/java/com/reeflog/reeflogapi/repository/AquariumRepository.java
+++ src/main/java/com/reeflog/reeflogapi/repository/AquariumRepository.java
@@ -1,12 +1,17 @@
 package com.reeflog.reeflogapi.repository;
 
 import com.reeflog.reeflogapi.beans.Aquarium;
+import com.reeflog.reeflogapi.beans.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AquariumRepository extends JpaRepository<Aquarium, Integer> {
 
     Aquarium findById(int id);
+
+    List<Aquarium> findAquariumsByMember(Member member);
 
 }
--- src/main/java/com/reeflog/reeflogapi/restcontroller/AquariumController.java
+++ src/main/java/com/reeflog/reeflogapi/restcontroller/AquariumController.java
@@ -8,16 +8,22 @@ import com.reeflog.reeflogapi.beans.ReefAquarium;
 import com.reeflog.reeflogapi.beans.helpers.ReefAquariumForm;
 import com.reeflog.reeflogapi.repository.AquariumRepository;
 import com.reeflog.reeflogapi.repository.MemberRepository;
+import com.reeflog.reeflogapi.security.JwtTokenUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class AquariumController {
 
     private static final Logger logger = LoggerFactory.getLogger((ReefLogApiApplication.class));
 
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
 
     @Autowired
     AquariumRepository aquariumRepository;
@@ -27,7 +33,7 @@ public class AquariumController {
 
 
     @PostMapping(value = "/api/addNewReefAquarium")
-    public ReefAquarium addNewReefAquarium(@RequestHeader("Authorization") String token, @RequestBody ReefAquariumForm reefAquariumForm){
+    public ReefAquarium addNewReefAquarium(@RequestHeader("Authorization") String token, @RequestBody ReefAquariumForm reefAquariumForm) {
         ReefAquarium newReefAquarium = new ReefAquarium();
 
         Member member = memberRepository.findById(reefAquariumForm.getMemberId());
@@ -40,15 +46,33 @@ public class AquariumController {
         newReefAquarium.setMainPopulation(reefAquariumForm.getMainPopulation());
         newReefAquarium.setTypeOfMaintenance(reefAquariumForm.getTypeOfMaintenance());
         aquariumRepository.save(newReefAquarium);
-        logger.info("Un nouvel aquarium a été enregistré " + newReefAquarium );
+        logger.info("Un nouvel aquarium a été enregistré " + newReefAquarium);
         return newReefAquarium;
     }
 
-    @GetMapping(value="/api/deleteAquarium/{id}")
-    public String deleteAquarium(@RequestHeader("Authorization") String token, @PathVariable int id){
+    @GetMapping(value = "/api/deleteAquarium/{id}")
+    public String deleteAquarium(@RequestHeader("Authorization") String token, @PathVariable int id) {
         Aquarium aquariumToDelete = aquariumRepository.findById(id);
         aquariumRepository.delete(aquariumToDelete);
         return "L'aquarium " + aquariumToDelete.getId() + " a bien été supprimé de la base";
     }
 
-}
+    @GetMapping(value = "/api/getListAquarium/{memberId}")
+    public List<Aquarium> getDetailAquarium(@RequestHeader("Authorization") String token, @PathVariable int memberId) {
+        List<Aquarium> listAquariums = new ArrayList<>();
+        try {
+            Member member = memberRepository.findById(memberId);
+            boolean isTokenValide = jwtTokenUtil.validateCustomTokenForMember(token, member);
+            if (!isTokenValide) {
+                listAquariums = null;
+            } else {
+                listAquariums = aquariumRepository.findAquariumsByMember(member);
+            }
+        } catch (Exception e) {
+            logger.error(String.valueOf(e));
+        }
+        logger.info("Envoi liste des aquariums : " + listAquariums);
+
+        return listAquariums;
+    }
+    }
