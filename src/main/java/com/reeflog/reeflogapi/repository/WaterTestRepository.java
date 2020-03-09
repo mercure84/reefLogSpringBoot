d130ed7987fb39560c91f98370e3bb48cbc2ab83
Mjuju84 <julien.marcesse@gmail.com>
2020-03-09 16:34:22 +0100

    getWaterList is working

--- src/main/java/com/reeflog/reeflogapi/repository/WaterTestRepository.java
+++ src/main/java/com/reeflog/reeflogapi/repository/WaterTestRepository.java
@@ -1,10 +1,13 @@
 package com.reeflog.reeflogapi.repository;
 
+import com.reeflog.reeflogapi.beans.Aquarium;
 import com.reeflog.reeflogapi.beans.WaterTest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WaterTestRepository extends JpaRepository<WaterTest, Integer> {
 
-
+List<WaterTest> findByAquarium(Aquarium aquarium);
 
 }
--- src/main/java/com/reeflog/reeflogapi/restcontroller/WaterTestController.java
+++ src/main/java/com/reeflog/reeflogapi/restcontroller/WaterTestController.java
@@ -12,12 +12,10 @@ import com.reeflog.reeflogapi.security.JwtTokenUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.List;
 
 @RestController
 public class WaterTestController {
@@ -55,9 +53,24 @@ public class WaterTestController {
         } catch (Exception e) {
             return null;
         }
-
         return null;
+    }
 
+    @GetMapping(value = "/api/getWaterTestList/{aquariumId}")
+    public List<WaterTest> getWaterTestList(@RequestHeader("Authorization") String token, @PathVariable int aquariumId) {
+        try {
+            Aquarium aquarium = aquariumRepository.findById(aquariumId);
+            Member member = aquarium.getMember();
+            boolean isTokenValide = jwtTokenUtil.validateCustomTokenForMember(token, member);
+            if (isTokenValide) {
+                return waterTestRepository.findByAquarium(aquarium);
+            }
+
+        } catch (Exception e) {
+
+            return null;
+        }
+        return null;
     }
 
 
